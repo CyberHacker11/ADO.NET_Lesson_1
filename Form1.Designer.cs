55c15374ffbd4f5ab7da476ff393aec3aa1b1cc4
Cyber Hacker <ilgar.hasanzade@gmail.com>
2021-11-24 22:17:09 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Lesson_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cbxAuthors = new System.Windows.Forms.ComboBox();
+            this.lbxLists = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxCategories = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbxBooks = new System.Windows.Forms.ComboBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSearch.Location = new System.Drawing.Point(428, 37);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(76, 21);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // cbxAuthors
+            // 
+            this.cbxAuthors.FormattingEnabled = true;
+            this.cbxAuthors.Location = new System.Drawing.Point(12, 37);
+            this.cbxAuthors.Name = "cbxAuthors";
+            this.cbxAuthors.Size = new System.Drawing.Size(90, 21);
+            this.cbxAuthors.TabIndex = 1;
+            this.cbxAuthors.SelectedIndexChanged += new System.EventHandler(this.cbxAuthors_SelectedIndexChanged);
+            // 
+            // lbxLists
+            // 
+            this.lbxLists.FormattingEnabled = true;
+            this.lbxLists.Location = new System.Drawing.Point(590, 15);
+            this.lbxLists.Name = "lbxLists";
+            this.lbxLists.Size = new System.Drawing.Size(198, 420);
+            this.lbxLists.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Authors";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(143, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Categories";
+            // 
+            // cbxCategories
+            // 
+            this.cbxCategories.FormattingEnabled = true;
+            this.cbxCategories.Location = new System.Drawing.Point(146, 37);
+            this.cbxCategories.Name = "cbxCategories";
+            this.cbxCategories.Size = new System.Drawing.Size(90, 21);
+            this.cbxCategories.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(278, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Books";
+            this.label3.UseWaitCursor = true;
+            // 
+            // cbxBooks
+            // 
+            this.cbxBooks.FormattingEnabled = true;
+            this.cbxBooks.Location = new System.Drawing.Point(278, 37);
+            this.cbxBooks.Name = "cbxBooks";
+            this.cbxBooks.Size = new System.Drawing.Size(90, 21);
+            this.cbxBooks.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbxBooks);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxCategories);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxLists);
+            this.Controls.Add(this.cbxAuthors);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cbxAuthors;
+        private System.Windows.Forms.ListBox lbxLists;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxCategories;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbxBooks;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { InitializeComponent(); }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            path = new SqlConnection("Data Source=CYBER-HACKER\\SQLEXPRESS;Initial Catalog=Library;Integrated Security=True;");
+            MultiQueries();
+
+        }
+
+        public void SelectQuery()
+        {
+            SqlDataReader reader = null;
+
+            try
+            {
+                path.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Authors", path);
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cbxAuthors.Items.Add(reader.GetString(1) + " " + reader.GetString(2));
+                }
+            }
+            finally
+            {
+                path?.Close();
+                reader?.Close();
+            }
+
+        }
+
+        public void MultiQueries()
+        {
+            try
+            {
+                path.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Authors; SELECT * FROM Categories; SELECT * FROM Books", path);
+                reader = cmd.ExecuteReader();
+
+                int step = 0;
+
+                do
+                {
+                    while (reader.Read())
+                    {
+                        if (step == 0) cbxAuthors.Items.Add(reader.GetString(1));
+                        else if (step == 1) cbxCategories.Items.Add(reader.GetString(1));
+                        else if (step == 2) cbxBooks.Items.Add(reader.GetString(1));
+                    }
+                    step++;
+
+                } while (reader.NextResult());
+            }
+            finally
+            {
+                path?.Close();
+                reader?.Close();
+            }
+        }
+        SqlDataReader reader = null;
+        SqlConnection path = null;
+
+        private void cbxAuthors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataReader reader = null;
+            lbxLists.Items.Clear();
+            try
+            {
+                path.Open();
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Books JOIN Authors ON Authors.Id = Books.Id_Author WHERE Authors.FirstName = @p1", path);
+
+                string aaa = cbxAuthors.SelectedItem.ToString();
+
+                cmd.Parameters.AddWithValue("@p1", aaa);
+
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read()) lbxLists.Items.Add(reader.GetString(1));
+            }
+            finally
+            {
+                path?.Close();
+                reader?.Close();
+            }
+        }
+    }
+}
